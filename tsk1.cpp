b209c2f7bc736cb7da238146b7c2773ceeda8285
alexhedgehogyeap <119707365+alexhedgehogyeap@users.noreply.github.com>
2023-12-14 19:35:43 +0300

    Update tsk1.cpp

--- tsk1.cpp
+++ tsk1.cpp
@@ -6,7 +6,7 @@ int main()
 {
 	double a, b, res;
 	printf("Type 2 numbers: ");
-	scanf("%lf %lf",  &a, &b);
+	scanf_s("%lf %lf",  &a, &b);
 	res = a + b;
 	printf("Sum = %.16g\n", res);
 	res = a - b;
